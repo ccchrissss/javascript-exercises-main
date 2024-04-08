8180a1b35bdec1d4d835cf836171dc58c3c1021f
Chris Nelson <chrisnelson7791@gmail.com>
2024-04-08 16:25:43 -0700

    palindromes section complete except for last test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  // const alphabetCaps = 'ABCDEFHIJKLMNOPQRSTUVWXYZ'.split('')
+  // str = str.toLowerCase().split('').filter( e => alphabet.includes(e) || alphabetCaps.includes(e) ).join('')
+  str = str.toLowerCase().split('').filter( e => alphabet.includes(e) ).join('')
+ 
+  let strRev = str.split('')
+  strRev = strRev.reverse().join('')
+
+  
+  console.log('strRev: ', strRev)
+  console.log('str: ', str)
+
+
+  return str === strRev ? true : false
 };
+
+// console.log(palindromes('abcdef'))
+console.log(palindromes('rac3e3car'))
+console.log(palindromes('r3ace3car'))
 
 // Do not edit below this line
 module.exports = palindromes;
