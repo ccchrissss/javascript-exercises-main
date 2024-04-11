645092c7a55719776f69f7bc2e38e10fc189e87c
Chris Nelson <chrisnelson7791@gmail.com>
2024-04-10 18:25:34 -0700

    fibonacci section complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(desiredSeqNum) {
+
+  if (desiredSeqNum < 0) return 'OOPS'
+
+  let fibArr = [0, 1]
+
+  for (let i = 1; i < desiredSeqNum; i++) {
+    
+    fibArr.push( fibArr[i] + fibArr[i - 1] )
+    
+  }
+
+  return fibArr[desiredSeqNum]
 
 };
 
